806980e31a98b2b971807fa5e345d94fa74a140e
bitcionia <85541189+bitcionia@users.noreply.github.com>
2021-11-24 13:12:08 +0530

    code commit

--- src/app/components/service/http.service.ts
+++ src/app/components/service/http.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import {  EventEmitter, Output } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable, Subject } from "rxjs";
+// import { HttpClient, HttpHeaders } from "@angular/common/http";
+// import { Observable, Subject } from "rxjs";
 import { Router } from "@angular/router";
 // import { ToastrService } from "ngx-toastr";
-import { map } from "rxjs/operators";
+// import { map } from "rxjs/operators";
 import * as pako from "pako";
+import { ToastrService } from "ngx-toastr";
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  errorCount:number=0;
+  tokenId: any;
   get<T>(arg0: string) {
     throw new Error('Method not implemented.');
   }
+  token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWQ2MzgxYmQzMDAyNzU2YmU4ODYxMiIsImlhdCI6MTYzNzcwNzAwNCwiZXhwIjoxNjM3NzkzNDA0fQ.ZiozAL5Fx7pxBC36YUYCIydcU_qeRToYpQyb8hthOc4"
 //LIVE
 baseURL: string = "https://www.bitconia.com/api/v1/";
 headers: any = new HttpHeaders({ "Content-Type": "application/json" });
+// headers = new HttpHeaders().set("Content-Type", "application/json")
+//       .set("Authorization",this.token)
 // formheaders: any = new HttpHeaders({ "Content-Type": "multipart/form-data" });
 userloginurl: string = "user/auth/login";
   logout: string ="user/auth/logout";
@@ -26,21 +35,46 @@ userloginurl: string = "user/auth/login";
   getprofile: string="user/auth/getprofile";
   resetpassword : string="user/auth/resetpassword";
   forgetpassword: string="user/auth/forgotpassword";
-  create_user: string="user/user/create";
-  googleathu : string="user/auth/g2f/enable";
-  constructor(
+  create_user: string="user/user/signup";
+  signupathu:string="user/auth/signup/sent/otp"
+  googleathu : string="user/auth/g2f/get";
+  googlenable : string="user/auth/g2f/enable";
+  googleverify: string="user/auth/g2f/verify";
+  googledisable : string="user/auth/g2f/disable";
+  loginverify: string="user/auth/verify/otp";
+  twofactor: string="user/auth/sent/otp";
+    constructor(
     public http: HttpClient,
     public router: Router,
-  ) { }
+    public toastr: ToastrService,
+
+  ) {
+    
+} 
+  errorCallBack(apiResp) {
+    if (!apiResp) {
+      // this.errorCount++;
+      /** spinner ends after 10 seconds */
+      if (this.errorCount < 1) {
+        // this.toastr.error("Something went wrong..Please try after somtime!")
+        this.toastr.error("Please try after somtime!", "", {
+          positionClass: "toast-bottom-right",
+          closeButton: true,
+          timeOut: 5000,
+        });
+      }
+    }
+  }
   getSessionToken() {
-    var tokenId = JSON.parse(localStorage.getItem("data"));
-    return tokenId;
+    this.tokenId = JSON.parse(localStorage.getItem("data"));
+    return this.tokenId;
   }
   // getAuthHeaders() {
   //   return this.headers.append(
   //     "Authorization", + this.getSessionToken()
   //   );
   // }
+  
   getAuthHeaders() {
     return this.headers.append(
       "Authorization", this.getSessionToken()
@@ -70,23 +104,44 @@ changePassword(jsonObj: any): Observable<any> {
     });
 }
 forgetPassword(jsonObj: any): Observable<any> {
-  return this.http.post(this.baseURL + this.forgetpassword, jsonObj, {
-    // headers: this.getAuthHeaders(),
-  });
+  return this.http.post(this.baseURL + this.forgetpassword, jsonObj);
 }
 resetPassword(jsonObj: any): Observable<any> {
-  return this.http.post(this.baseURL + this.resetpassword, jsonObj, {
-    headers: this.getAuthHeaders(),
-  });
+  return this.http.post(this.baseURL + this.resetpassword, jsonObj);
 }
 createuser(jsonObj: any): Observable<any> {
-  return this.http.post(this.baseURL + this.create_user, jsonObj, {
+  return this.http.post(this.baseURL + this.create_user, jsonObj);
+    // ...
+    // this.headers.set(key, base);  });
+}
+g2fa(): Observable<any> {
+  return this.http.get(this.baseURL + this.googleathu, {
     headers: this.getAuthHeaders(),
   });
 }
-g2fa(jsonObj: any): Observable<any> {
-  return this.http.post(this.baseURL + this.googleathu, jsonObj, {
+g2faenable(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.googlenable, jsonObj, {
     headers: this.getAuthHeaders(),
   });
 }
+g2fverify(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.googleverify, jsonObj, {
+    headers: this.getAuthHeaders(),
+  });
 }
+signupotp(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.signupathu, jsonObj);
+}
+loginotp(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.loginverify, jsonObj);
+}
+twofactorotp(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.twofactor, jsonObj);
+}
+twofactorver(jsonObj: any): Observable<any> {
+  return this.http.post(this.baseURL + this.loginverify, jsonObj);
+}
+}
+
+
+
